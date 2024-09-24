5e623b7c2cd17d9aee6043c000bc46dfd4d25c08
Saksham4202 <101286313+Saksham4202@users.noreply.github.com>
2024-09-24 13:17:48 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import TextEditor from './components/TextEditor';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Advanced React Text Edit Tool</h1>
+      <TextEditor />
+    </div>
+  );
+}
+
+export default App;
